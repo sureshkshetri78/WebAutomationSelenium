abb68b73282aa4ef48fa9ef99d00785f6b03646d
Suresh Basnet <sureshkshetri78@gmail.com>
2019-09-23 13:19:33 -0400

    Switching between parent frame and child frame

--- src/test/IframeSwitching.java
+++ src/test/IframeSwitching.java
@@ -7,19 +7,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class IframeSwitching {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "D:\\Training QA\\Selenium Training\\Driver\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://seleniumautomationpractice.blogspot.com/2019/07/example-of-html-iframe-alternative.html");
 		driver.switchTo().frame("ID_Frame1");
 		driver.findElement(By.name("ts_first_name")).sendKeys("suresh");
 		driver.findElement(By.name("ts_last_name")).sendKeys("basnet");
+		Thread.sleep(3000);
 		
 		driver.switchTo().parentFrame();
 		WebElement frame = driver.findElement(By.id("ID_Frame2"));
 		driver.switchTo().frame(frame);
-		driver.findElement(By.name("ts_first_name")).sendKeys("qqqqq");
-		driver.findElement(By.name("ts_last_name")).sendKeys("zzzzz");
+		driver.findElement(By.name("firstname")).sendKeys("qqqqq");
+		driver.findElement(By.name("lastname")).sendKeys("zzzzz");
 	}
 
 }
